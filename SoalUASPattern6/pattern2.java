00c596afb1232a61ae6d10a0eabd3b3670b023e9
TubagusMuhamadIhsanFebrian <tubagusihsan28@gmail.com>
2026-01-19 10:27:21 +0700

    SoalUASPattern6

---
+++ SoalUASPattern6/pattern2.java
@@ -0,0 +1,17 @@
+public class pattern2 {
+    public static void main(String[] args) {
+        int n = 5;
+        int i, j;
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i - 1; j++) {
+                System.out.print("  ");
+            }                
+            for (j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
